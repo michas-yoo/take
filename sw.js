80df2129a00e70ff924c7dd80d40c8d89a6d6012
Michas <michas-yoo@list.ru>
2017-06-14 22:19:52 +0300

    added mobile responsibility & caching

---
+++ sw.js
@@ -0,0 +1,45 @@
+var cacheName = "taptap", filesToCache = [
+"css/style.css",
+"img/apple-logo.png",
+"img/car-interior.png",
+"img/dawn.png",
+"img/girl.png",
+"img/iphone.png",
+"img/logo-round.png",
+"img/main-bg.png",
+"img/windows-logo.png",
+"img/wtf-guy.png"];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
